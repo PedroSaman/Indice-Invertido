93979d7debda52ab752adbb0230d5744e68df2be
Pedro Saman <pedrosaman96@gmail.com>
2016-11-19 23:10:32 -0200

    Update main.c

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "TAD.h"
+#include "TAD_HS.h"
+#include "TAD_HA.h"
+#include "TAD_GERAL.h"
 
 int main(int argc, char *argv[]){
 
-	char Hash_Escolhido[13];
 	int x, Tamanho_Max;
 	struct rusage uso_inicio, uso_final;    
 	struct timeval inicio, fim;             /*Variáveis para calcular tempo de execução e uso de memória*/
@@ -23,8 +24,7 @@ int main(int argc, char *argv[]){
 				break;
 			case 'n':
 				if (strcmp (optarg,"HS") == 0){
-					memcpy(Hash_Escolhido, "Hash Simples", 13);
-					printf("%s\n",Hash_Escolhido );
+					printf("Hash Simples\n");
 					gettimeofday(&inicio, NULL);
 					CriaIndiceInvertidoHS();
 					gettimeofday(&fim, NULL);
@@ -33,13 +33,20 @@ int main(int argc, char *argv[]){
 					printf("\tTempo de execução %.7lfs\n", difftime(fim.tv_sec, inicio.tv_sec)
 																+ (double) (fim.tv_usec - inicio.tv_usec)/1000000);
 				}else if(strcmp (optarg,"HA") == 0){
-					//CriaIndiceInvertidoHA(Tamanho_Max );
+					printf("Hash Árvore\n");
+					gettimeofday(&inicio, NULL);
+					CriaIndiceInvertidoHA();
+					gettimeofday(&fim, NULL);
+					getrusage(RUSAGE_SELF, &uso_final);
+					printf("\n\n\tMemória usada em Kbytes: %ld\n", uso_final.ru_maxrss);
+					printf("\tTempo de execução %.7lfs\n", difftime(fim.tv_sec, inicio.tv_sec)
+																+ (double) (fim.tv_usec - inicio.tv_usec)/1000000);
 				}
 				break;
 			case 'h':
 					system("clear");
 					printf("***************************************************\n***********************Ajuda***********************\n***************************************************\n");
-					printf("\t -n Informa qual TAD será utilizado. Disponíveis:\n\tHash Aberto Simples (HS).\n\t Hash Arvore (HA).\t");
+					printf("\t -n Informa qual TAD será utilizado. Disponíveis:\n\tHash Aberto Simples (HS).\n\tHash Arvore (HA).\t");
 					printf("\n\t -t Informa qual o tamanho da maior palavra dentro dos textos\n");
 				break;			
 			default:
